ed8e7ed562cf94fbe8d361423cfe18844356c57d
ASJ THILAK <78794112+ASJTHILAK@users.noreply.github.com>
2022-04-13 10:30:05 +0530

    Update supermarket.java

--- DAY 2 (12.04.22)/supermarket.java
+++ DAY 2 (12.04.22)/supermarket.java
@@ -2,11 +2,19 @@
 package supermarket;
 import java.util.*;
 
-class Fruits {
+class Produce {
+    double discount = 0.1;
     double rate;
-    double discount = 0.18;
     double offer;
-    public void print_A() { System.out.println("Class A"); }
+}
+
+class Fruits extends Produce{
+    Fruits(){
+        discount = 0.18;
+        if(this.discount>discount){
+            discount = this.discount;
+        }
+    }
 }
  
 class Apple extends Fruits {
@@ -18,7 +26,6 @@ class Apple extends Fruits {
     public double amount(String a,String b){
         b = b.replaceAll("\\D","");
         double qt = Double.parseDouble(b);
-        System.out.println(qt);
         if(qt==discount){
             return rate*qt;
         }
@@ -48,8 +55,56 @@ class Orange extends Fruits {
     }
 }
 
+
+class Veg extends Produce{
+    Veg(){
+        discount = 0.05;
+        if(this.discount>discount){
+            discount = this.discount;
+        }
+    }
+}
+
+class Potato extends Veg {
+    Potato(){
+        rate = 30;
+        discount = 5; // BUY 3
+        offer = 2;    // GET 1
+    }
+    public double amount(String a,String b){
+        b = b.replaceAll("\\D","");
+        double qt = Double.parseDouble(b);
+        if(qt==discount){
+            return rate*qt;
+        }
+        double count = discount;
+        while(count<qt){
+            qt = qt-offer;
+            count = count + discount;
+        }
+        double sum = (rate*qt);
+        return sum;
+    }
+}
+
+class Tomato extends Veg {
+    Tomato(){
+        rate = 70;
+        discount = 0.1;
+        if(this.discount>discount){
+            discount = this.discount;
+        }
+    }
+    public double amount(String a,String b){ 
+        b = b.replaceAll("\\D","");
+        double qt = Double.parseDouble(b);
+        double sum = (rate*qt)-(rate*qt*discount);
+        return sum;
+    }
+}
+
 public class Supermarket {
-    
+    static double total =0;
     static String arrayStr[];
     
     static void bill(String a,String b){
@@ -58,12 +113,28 @@ public class Supermarket {
             double c = obj.amount(a,b);
             System.out.println("Price of Apple");
             System.out.println(c);
+            total += c;
         }
         else if("Orange".equals(a)){
             Orange obj = new Orange();
             double c = obj.amount(a,b);
             System.out.println("Price of Orange");
             System.out.println(c);
+            total += c;
+        }
+        else if("Potato".equals(a)){
+            Potato obj = new Potato();
+            double c = obj.amount(a,b);
+            System.out.println("Price of Potato");
+            System.out.println(c);
+            total += c;
+        }
+        else if("Tomato".equals(a)){
+            Tomato obj = new Tomato();
+            double c = obj.amount(a,b);
+            System.out.println("Price of Tomato");
+            System.out.println(c);
+            total += c;
         }    
     }
     
@@ -81,9 +152,10 @@ public class Supermarket {
                     Scanner sc= new Scanner(System.in);
                     String str = sc.nextLine();
                     arrayStr = str.split("[ ,]+");
-                    for(int i=0;i<(arrayStr.length)/2;i+=2){
+                    for(int i=0;i<(arrayStr.length);i+=2){
                         bill(arrayStr[i],arrayStr[i+1]);
                     }
+                    System.out.println("Total amount : "+total);
                     continue;
                 case 2:
                     break;
